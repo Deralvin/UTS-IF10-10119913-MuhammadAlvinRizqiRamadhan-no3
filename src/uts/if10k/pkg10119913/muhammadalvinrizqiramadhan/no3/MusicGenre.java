fce62256e8e4ee9ef8db86ece13b18686b539626
Dilan <ramadhanrizqi21@gmail.com>
2020-12-04 21:10:31 +0700

    UTS 3 first

---
+++ src/uts/if10k/pkg10119913/muhammadalvinrizqiramadhan/no3/MusicGenre.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10k.pkg10119913.muhammadalvinrizqiramadhan.no3;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MusicGenre {
+    String artisName;
+
+    public void setArtisName(String artisName) {
+        this.artisName = artisName;
+    }
+
+    public String getArtisName() {
+        return artisName;
+    }
+     
+}
